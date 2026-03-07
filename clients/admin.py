e66d48795820ec0e1a85b53007be4cfd69ae31f8
Anailton Silva <francisco.anailton17@gmail.com>
2026-03-07 17:49:23 -0300

    feat: adds client and vehicle models, configures admin and adjusts URL settings

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client, Vehicle
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone_number')
+
+
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ('client', 'vehicle_brand', 'vehicle_model', 'vehicle_year')
--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Vehicle(models.Model):
+    client = models.ForeignKey(Client, related_name='vehicles', on_delete=models.CASCADE)
+    vehicle_model = models.CharField(max_length=100, blank=True, null=True)
+    vehicle_brand = models.CharField(max_length=100, blank=True, null=True)
+    vehicle_year = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.vehicle_brand} {self.vehicle_model} ({self.vehicle_year})"
+
