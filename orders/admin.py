77e07aa3c076e76a2d8b2f99a3b0358032047314
Anailton Silva <francisco.anailton17@gmail.com>
2026-03-18 15:51:34 -0300

    feat: implement Order and OrderItem models, add admin registration and search functionality

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderItem
+
+admin.site.register(Order)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'quantity', 'unit_price')
+
+
+admin.site.register(OrderItem, OrderItemAdmin)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('number', 'client', 'vehicle', 'order_date', 'status', 'total')
+    list_filter = ('status', 'order_date')
+    search_fields = ('number', 'client__name', 'vehicle__model')  
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+from clients.models import Client, Vehicle
+
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('open', 'Aberto'),
+        ('progress', 'Em Progresso'),
+        ('done', 'Finalizado'),
+    ]
+    number = models.CharField(max_length=20, unique=True, blank=True)
+    client = models.ForeignKey(Client, on_delete=models.PROTECT)
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.PROTECT)
+    order_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=12, choices=STATUS_CHOICES, default='open')
+    description = models.TextField(blank=True, null=True)
+
+    @property
+    def total(self):
+        # Calcula o total do pedido somando o preço de cada item multiplicado pela quantidade
+        return sum(item.quantity * item.unit_price for item in self.items.all())
+    
+    def save(self, *args, **kwargs):
+
+        if not self.number:
+            last_order = Order.objects.order_by('-id').first()
+
+            if last_order and last_order.number:
+                last_number = int(last_order.number.split('-')[1])
+                new_number = last_number + 1
+            else:
+                new_number = 1
+
+            self.number = f"OS-{new_number:04d}"
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Order {self.number} - {self.client.name}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order,related_name='items',on_delete=models.CASCADE)
+    # Product = models.ForeignKey(Client, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField(default=1)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} - {self.price}"
