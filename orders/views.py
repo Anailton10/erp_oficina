a69f102cd86a7ec58bb979211232afc022e6d36d
Anailton Silva <francisco.anailton17@gmail.com>
2026-03-21 12:34:48 -0300

    feat: refatorar base.html and views.py to improve order structure and functionality

--- orders/views.py
+++ orders/views.py
@@ -1,61 +1,72 @@
-from django.http import Http404
+import json
+from django.http import Http404, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
-from django.views import View,generic
-
+from django.views import View, generic
 from clients.models import Client, Vehicle
-
-from .models import Order
-from .forms import OrderForm, OrderItemFormSet
+from products.models import CatalogItem
+from .forms import OrderForm, OrderItemForm
+from .models import Order, OrderItem
 
 
 class OrderCreateView(View):
     def get(self, request, client_id, vehicle_id):
-        client = get_object_or_404(Client, id=client_id) # Substitua pelo ID do cliente desejado
-        vehicle = get_object_or_404(Vehicle, id=vehicle_id) # Substitua pelo ID do veículo desejado
-
-        if vehicle.client != client:
-            raise Http404("Veículo não pertence ao cliente")
-
-        form = OrderForm()
-        formset = OrderItemFormSet()
-
-        context = {
-        'form': form,
-        'formset': formset,
-        'client': client,
-        'vehicle': vehicle
-        }
-        return render(request, 'orders/order_form.html', context)
-    
-    def post(self, request, client_id, vehicle_id):
-
         client = get_object_or_404(Client, id=client_id)
         vehicle = get_object_or_404(Vehicle, id=vehicle_id)
+        if vehicle.client != client:
+            raise Http404("Veículo não pertence ao cliente")
+        form = OrderForm()
+        return render(request, 'orders/order_form.html', {
+            'form': form,
+            'client': client,
+            'vehicle': vehicle,
+        })
 
+    def post(self, request, client_id, vehicle_id):
+        client = get_object_or_404(Client, id=client_id)
+        vehicle = get_object_or_404(Vehicle, id=vehicle_id)
         form = OrderForm(request.POST)
-        formset = OrderItemFormSet(request.POST)
-
-        if form.is_valid() and formset.is_valid():
-
+        if form.is_valid():
             order = form.save(commit=False)
             order.client = client
             order.vehicle = vehicle
             order.save()
-
-            formset.instance = order
-            formset.save()
-
-            return redirect('order_detail', pk=order.pk)
-        context = {
+            return redirect('orders:order_detail', pk=order.pk)
+        return render(request, 'orders/order_form.html', {
             'form': form,
-            'formset': formset,
             'client': client,
-            'vehicle': vehicle
-        }
+            'vehicle': vehicle,
+        })
 
-        return render(request, 'orders/order_form.html', context)
 
 class OrderListView(generic.ListView):
     model = Order
     template_name = 'orders/order_list.html'
     context_object_name = 'orders'
+
+
+class OrderDetailView(generic.DetailView):
+    model = Order
+    template_name = 'orders/order_detail.html'
+    context_object_name = 'order'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['catalog_items'] = CatalogItem.objects.all().order_by('type', 'name')
+        return context
+
+
+class AddOrderItemView(View):
+    def post(self, request, order_id):
+        try:
+            order = Order.objects.get(id=order_id)
+            data = json.loads(request.body)
+            product = CatalogItem.objects.get(id=data['product_id'])
+            OrderItem.objects.create(
+                order=order,
+                product=product,
+                quantity=int(data['quantity']),
+                unit_price=data['unit_price'],
+            )
+            return JsonResponse({'success': True})
+        except Exception as e:
+            return JsonResponse({'success': False, 'error': str(e)})
