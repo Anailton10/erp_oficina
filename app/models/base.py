c7313836539b6673a21ff9698da0f96cfba5c077
Anailton Silva <francisco.anailton17@gmail.com>
2026-03-24 14:11:43 -0300

    feat:adding soft_delete to BaseModel

--- app/models/base.py
+++ app/models/base.py
@@ -4,8 +4,13 @@ from django.db import models
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    is_active = models.BooleanField(default=True) # Adiciona o campo de status ativo
+    # Adiciona o campo de status ativo
+    is_active = models.BooleanField(default=True)
+
+    def soft_delete(self):
+        self.is_active = False
+        self.save()
 
     class Meta:
         abstract = True
-        ordering = ['-created_at']
+        ordering = ["-created_at"]
--- products/models.py
+++ products/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+from app.models.base import BaseModel
 
-class CatalogItem(models.Model):
+
+class CatalogItem(BaseModel):
     TYPE_CHOICES = [
-        ('PRODUTO', 'produto'),
-        ('SERVICO', 'serviço'),
+        ("PRODUTO", "produto"),
+        ("SERVICO", "serviço"),
     ]
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
@@ -14,25 +16,23 @@ class CatalogItem(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def has_stock_control(self):
-        return self.type == 'PRODUTO'
-      
+        return self.type == "PRODUTO"
+
     def clean(self):
         # Validação para garantir que produtos tenham estoque e serviços não tenham estoque
         from django.core.exceptions import ValidationError
 
         # Se for um produto, deve ter estoque definido
-        if self.type == 'PRODUTO' and self.stock is None:
-            raise ValidationError({'stock': 'Produto precisa de estoque.'})
-        
+        if self.type == "PRODUTO" and self.stock is None:
+            raise ValidationError({"stock": "Produto precisa de estoque."})
+
         # Se for um serviço, não deve ter estoque definido
-        if self.type == 'SERVICO' and self.stock is not None:
-            raise ValidationError({
-                'stock': 'Serviço não deve ter estoque.'
-            })
-    
+        if self.type == "SERVICO" and self.stock is not None:
+            raise ValidationError({"stock": "Serviço não deve ter estoque."})
+
     def save(self, *args, **kwargs):
         # Garantir que a validação seja chamada antes de salvar
-        self.full_clean() # Chama o método de validação antes de salvar
+        self.full_clean()  # Chama o método de validação antes de salvar
         super().save(*args, **kwargs)
 
     def __str__(self):
